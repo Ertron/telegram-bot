6ac8afb595e16bd98b5841de35f160f7b38cd5f5
Ertron <ertron@veldrin.kiev.ua>
2018-01-24 18:10:14 +0200

    space

--- lib/CommandListInterface.php
+++ lib/CommandListInterface.php
@@ -6,6 +6,7 @@ interface CommandListInterface{
 
 	/**
 	 * @param string $string Command name
+	 *
 	 * @return Command instance of abstract class Command
 	 */
 	public static function getCommand(string $string);
